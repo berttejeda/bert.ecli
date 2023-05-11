55a840a788701f12908a1cc9509393d4ff876a47
Bert Tejeda <etejeda@tecknicos.com>
2023-05-10 21:16:28 -0400

    Changed package name

--- setup.py
+++ setup.py
@@ -10,19 +10,22 @@ def package_files(directory):
             paths.append(os.path.join('..', path, filename))
     return paths
 
-extra_files = package_files('ecli/plugins')
+extra_files = package_files('ecli.plugins')
 
 required_packages = [
-        'bs4>=0.0.1,<0.1',
-        'click>=8.0,<9.0',
-        'click-plugins>=1.1.1,<1.2',
-        'colorama>=0.4.3,<0.5',
-        'first>=2.0.2,<2.1',
-        'bertdotconfig>=4.2.0,<4.3.0',
-        'paramiko>=2.7.0,<2.8',
-        'jello>=1.2.10,<1.3',
-        'requests>=2.22.0,<2.23',
-        'PyYAML>=5.3.1,<6.0',
+        'bertdotconfig>=4.3.0,<4.4.0',
+        'bs4>=0.0.1,<0.1.0',
+        'click>=8.1.3,<9.0.0',
+        'click-plugins==1.1.1',
+        'colorama==0.4.3',
+        'first==2.0.2',
+        'jello==1.2.10',
+        'lxml==4.9.1',
+        'pandas>=2.0.1,<3.0.0',
+        'paramiko>=2.11.0,<3.0.0',
+        'PyCryptodome>=3.17,<4.0.0',
+        'PyYAML>=6.0,<7.0.0',
+        'requests>=2.28.1,<2.30.0',
     ]
 
 if '--show-packages' in ' '.join(sys.argv):
@@ -31,7 +34,7 @@ if '--show-packages' in ' '.join(sys.argv):
     sys.exit()    
 
 setup(
-    name='bertdotecli',
+    name='bert-dot-ecli',
     version='1.6.1',
     packages=find_packages(),
     include_package_data=True,
