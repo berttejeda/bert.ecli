a094137504297d0c63eec3c6ddca6574effb4014
Bert Tejeda <etejeda@tecknicos.com>
2023-05-12 14:20:17 -0400

    Fixed version mismatch

--- btecli/cli.py
+++ btecli/cli.py
@@ -15,7 +15,7 @@ from btconfig import Config
 
 # Private variables
 __author__ = 'Bert Tejeda'
-__version__ = '1.6.2'
+__version__ = '1.6.3'
 # Configuration Files
 config_file_name = 'ecli.config.yaml'
 # Initialize App Config
